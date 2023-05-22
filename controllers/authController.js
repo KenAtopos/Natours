e8df81a7deae95ab85e9d4f09ddb55e56ced0746
Ken Atopos <z1158229761@outlook.com>
2023-05-23 04:03:44 +0800

    implement forget password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const { promisify } = require("util");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const sendEmail = require("../utils/email");
 
 // eslint-disable-next-line arrow-body-style
 const signToken = (id) => {
@@ -114,5 +115,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3. send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/users/resetPassword/${resetToken}`;
+  console.log(resetURL);
+
+  const message = `Forget your password? Reset in ${resetURL}\nIf you didn't forget the password, please ignore this email.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token(valid for 10 min)",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email",
+    });
+  } catch (err) {
+    console.log(err);
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError("There was an error sending email. Try again later"),
+      500
+    );
+  }
 });
+
 exports.resetPassword = (req, res, next) => {};

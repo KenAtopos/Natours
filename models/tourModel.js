7091bfd7b75c194e9893ff078c1b257ac54ac02a
Ken Atopos <z1158229761@outlook.com>
2023-05-25 00:50:06 +0800

    populate the data

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
-const validator = require("validator");
-const User = require("./userModel");
+// const validator = require("validator");
+// const User = require("./userModel");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -105,7 +105,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -123,12 +128,13 @@ tourSchema.pre("save", function (next) {
   next();
 });
 
-tourSchema.pre("save", async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
+// embedding the user to the tour schema
+// tourSchema.pre("save", async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
 
-  next();
-});
+//   next();
+// });
 
 // tourSchema.pre("save", function (next) {
 //   console.log("will save document...");
@@ -144,6 +150,14 @@ tourSchema.pre("save", async function (next) {
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
   next();
 });
 

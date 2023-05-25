fc679b9cb4b281eb04b9786b324516de67e87c4d
Ken Atopos <z1158229761@outlook.com>
2023-05-26 02:38:30 +0800

    start to work with pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -14,7 +15,13 @@ const reviewRouter = require("./routers/reviewRoutes");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 // 1) global middleware
+// serving static files
+app.use(express.static(path.join(__dirname, "public")));
+
 // set security HTTP headers
 app.use(helmet());
 
@@ -59,9 +66,6 @@ app.use(
   })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -69,6 +73,13 @@ app.use((req, res, next) => {
 });
 
 // 3) routes
+app.get("/", (req, res) => {
+  res.status(200).render("base", {
+    tour: "The forest hiker",
+    user: "Ken",
+  });
+});
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
